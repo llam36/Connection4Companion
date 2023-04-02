db56683ac41d32dfb11d38c659cd6e74ef0be13d
Daiwik Pal <daiwikpal123@gmail.com>
2023-04-01 21:25:40 -0400

    add POST request to user API endpoint

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,4 +1,31 @@
+import mongoose from "mongoose"
+import User from "../../server/mongodb/models/exampleModel.js"
+
+
 export default function handler(req, res) {
 
-    return res.status(200).json({"user": true});
+    const requestMethod = req.method; 
+    const body = res.body; 
+
+    switch(requestMethod){
+        case 'POST':
+
+        try{
+
+            const newUser = new User(body.userData); 
+            newUser.save(); 
+            return res.status(200).send("User added to DB"); 
+
+        }catch(e){
+            console.log(e)
+            return res.status(200).send("Unable to add user to DB"); 
+
+        }
+
+    }
+    //const body = 
+
+    
+
+    // return res.status(200).json({"user": true});
 }
