8b25810fde773700baa401090e301d8a1d12ec5d
Uma <uma2005anand@gmail.com>
2023-04-04 12:06:59 -0400

    feat: added animal-user verification to training log and created validators for name and email

--- src/pages/api/training.js
+++ src/pages/api/training.js
@@ -1,9 +1,27 @@
 import TrainingLog from "server/mongodb/models/traininglog.js"
 import { connectDB } from "../../../server/mongodb";
+import mongoose from "mongoose"
+import trainingLogChecker from "../infovalidation";
+
 
 export default async function handler(req, res) {
     let data = req.body;
     connectDB();
+    if (trainingLogChecker(data) != 1) {
+        return res.status(400).json({errorType: trainingLogChecker(data)})
+    }
+    try {
+        let animal = await TrainingLog.findById(data.animal);
+        let owner = animal.owner;
+        if (owner != data.user) {
+            return res.status(400).json({errorType: "Given user is not the animal owner."})
+        }
+    } catch (e) {
+        if (e.name == "ValidationError") {
+            return res.status(400).json({errorType: "Invalid data."});
+        }
+        return res.status(500).json({errorType: "Server side error"})
+    }
     try {
         const newTrainingLog = new TrainingLog(data);
         await newTrainingLog.save();
---
+++ src/pages/infovalidation.js
@@ -0,0 +1,36 @@
+function nameChecker(name) {
+    return /^[a-zA-Z]+$/.test(name);
+}
+
+function emailChecker(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+
+function hoursChecker(numHours) {
+    return numHours >= 0;
+}
+
+function userChecker(data) {
+    if (!nameChecker(data.firstName) || !nameChecker(data.lastName)) {
+        return "Name contains invalid characters.";
+    } else if (!emailChecker(data.email)) {
+        return "Invalid email address.";
+    }
+    return 1;
+}
+
+function animalChecker(data) {
+    if (!nameChecker(data.name)) {
+        return "Name contains invalid characters.";
+    } else if (!hoursChecker(data.hoursTrained)) {
+        return "Invalid number of hours entered.";
+    }
+    return 1;
+}
+
+function trainingLogChecker(data) {
+    if (!hoursChecker(data.hours)) {
+        return "Invalid number of hours entered.";
+    }
+    return 1;
+}
