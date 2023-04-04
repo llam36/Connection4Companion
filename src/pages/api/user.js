0241a8f374fc19bdadda871fc08aea9dde3be11e
Daiwik Pal <daiwikpal123@gmail.com>
2023-04-04 19:15:13 -0400

    fixed user.js

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { connectDB } from "../../../server/mongodb";
 import User from "../../server/mongodb/models/exampleModel.js";
 
 export default function handler(req, res) {
   const requestMethod = req.method;
   const body = res.body;
+  connectDB();
 
   switch (requestMethod) {
     case 'POST':
@@ -12,7 +14,10 @@ export default function handler(req, res) {
         newUser.save();
         return res.status(200).send("User added to DB");
       } catch (e) {
-        console.log(e);
+          if (e.name == "ValidationError") {
+            return res.status(400).json({errorType: "Invalid data."});
+          }
+          return res.status(500).json({errorType: "Server side error"})
       }
-  }
+      }
 }
