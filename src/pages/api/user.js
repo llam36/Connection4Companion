5a813f95e9e11b0f9631747ef2525d50db3cb01e
Uma <uma2005anand@gmail.com>
2023-04-06 19:32:38 -0400

    chore: tested endpoints

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,24 +1,23 @@
-import mongoose from "mongoose";
 import User from "server/mongodb/models/user.js";
 import { connectDB } from "../../../server/mongodb";
 
 export default function handler(req, res) {
   const requestMethod = req.method;
   const body = req.body;
-  console.log(body.userData);
+  console.log(body);
   connectDB();
 
   switch (requestMethod) {
     case 'POST':
       try {
-        const newUser = new User(body.userData);
+        const newUser = new User(body);
         newUser.save();
         return res.status(200).send("User added to DB");
       } catch (e) {
           if (e.name == "ValidationError") {
             return res.status(400).json({errorType: "Invalid data."});
           }
-          return res.status(500).json({errorType: "Server side error"})
+          return res.status(500).json({errorType: e.message})
       }
       }
 }
