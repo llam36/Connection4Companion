34d2e3d6a1f498c89bebf5253371f1269a11cbbe
llam36 <llam36@gatech.edu>
2023-04-10 14:21:06 -0400

    fix: create login func in login.js for external use

--- src/pages/api/user/login.js
+++ src/pages/api/user/login.js
@@ -4,11 +4,19 @@ import User from "server/mongodb/models/user.js";
 
 export default async function handler(req, res) {
     const { email, password } = req.body;
+    const result = await login(email, password);
+    if (!result.success) {
+        return res.status(403).json(result);
+    }
+    return res.status(200).json(result);
+}
+
+export async function login(email, password) {
     connectDB();
     const user = await User.findOne({ email });
     const result = await bcrypt.compare(password, user.password);
     if (!result) {
-        return res.status(403).json({ success: false, message: "Incorrect email or password" });
+        return { success: false, message: "Incorrect email or password" };
     }
-    return res.status(200).json({ success: true, message: "Successfully log in" });
+    return { success: true, message: "Successfully log in" };
 }
