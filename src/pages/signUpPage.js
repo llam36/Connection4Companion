a2f2cf87a6167f6c4ddce8358ef46a08ec59bad9
Daiwik Pal <daiwikpal123@gmail.com>
2023-04-13 14:11:03 -0400

    feat: add create User Page to Main UI

---
+++ src/pages/signUpPage.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import RegistrationForm from './createUserPage'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+    return (
+        <div>
+          <h1> Create your account: </h1>
+          <RegistrationForm />
+        </div>
+      )
+
+}
