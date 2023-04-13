c7ba8fd7a45fb5c680d134bed02e7d934e20fbb0
Daiwik Pal <daiwikpal123@gmail.com>
2023-04-13 18:53:40 -0400

    feat: login page using verify.js

--- src/pages/loginUserForm.js
+++ src/pages/loginUserForm.js
@@ -5,7 +5,10 @@ function RegistrationForm() {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
+    admin: false, 
   });
+
+  const [isSignedIn, setSignedIn] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -19,12 +22,13 @@ function RegistrationForm() {
     const body = {
       email: formData.email,
       password: formData.password,
+      admin: formData.admin
     };
 
-
     try{
-        const res = axios.post("/api/user/login", body);
+        const res = axios.post("/api/user/verify", body);
         console.log(res);
+        setSignedIn(true)
     } catch (e) {
         console.log(e);
     }
@@ -62,8 +66,25 @@ function RegistrationForm() {
         />
         </div>
 
+        <div class = "form-group">
+      <label>
+        Admin:
+        </label>
+        <input
+          type="checkbox"
+          name="admin"
+          value={formData.admin}
+          class="input"
+          onChange={handleInputChange}
+        />
+        </div>
+
       <br />
       <button href="/index" type="submit" class = "search">Submit</button>
+      <div>
+        {isSignedIn ? <p>Successfully Signed In </p> : <p> Not Signed In Yet</p>}
+      </div> 
+      
       </center>
     </form>
   );
