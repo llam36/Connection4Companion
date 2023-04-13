de1bc183188123414e5f37a63ef09a9044f2d01f
llam36 <llam36@gatech.edu>
2023-04-13 18:05:33 -0400

    feat: protect all routes with password

--- src/pages/api/training.js
+++ src/pages/api/training.js
@@ -1,13 +1,18 @@
 import TrainingLog from "server/mongodb/models/traininglog.js"
 import { connectDB } from "../../../server/mongodb";
 import { trainingLogChecker } from "../infovalidation";
-
+import { verifyJWT } from "./user/verify";
 
 export default async function handler(req, res) {
+    try {
+        const decodedJWT = verifyJWT(req);
+    } catch (e) {
+        return res.status(400).json({ success: false, message: e.message });
+    }
     let data = req.body;
     await connectDB();
-    
-    switch(req.method) {
+
+    switch (req.method) {
         case 'POST':
             let checker = await trainingLogChecker(data);
             if (!checker.success) {
