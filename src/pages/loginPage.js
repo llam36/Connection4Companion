09cf7d9c09c4ca8377fad6eea450887aa849f70e
Daiwik Pal <daiwikpal123@gmail.com>
2023-04-13 15:02:53 -0400

    feat: add login page features

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 //import styles from '@/styles/Home.module.css'
-import RegistrationForm from './createUserPage'
+import RegistrationForm from './loginUserForm'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,15 +10,14 @@ export default function Home() {
 
     return (
         <div>
-          <center>
-          <h1> Create your account: </h1>
+        <center>
+          <h1> Log in to your account: </h1>
           <RegistrationForm />
           <div>
-            <h2> Already have an account? </h2>
-            <a href="./login" class="card__button"> Login </a>
+            <h2> Don't have an account? </h2>
+            <a href="./signUpPage" class="card__button"> Login </a>
             <h2> Back to Home</h2>
             <a href="./" class="card__button"> Back to Home </a>
-
           </div>
           </center>
           
